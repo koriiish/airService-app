91da71e03dedea1f843139f742438efd3eb94616
Karrie <karinad000@gmail.com>
2024-06-18 18:52:16 +0300

    Custom colors, icons and font added to project

---
+++ aviaService/aviaService/aviaServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  aviaServiceApp.swift
+//  aviaService
+//
+//  Created by Карина Дьячина on 18.06.24.
+//
+
+import SwiftUI
+
+@main
+struct aviaServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
